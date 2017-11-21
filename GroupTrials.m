d490c11ab9dc2985918308cff2c7059c739f3d01
BrainLab1 <bahareh.taghizadeh@gmail.com>
2017-11-21 13:08:02 +0330

    new condition added for uncertainty task 'CuePos & TargetPos & CurrEV & CurrVar (uncertainty 6 conditions)'

--- GroupTrials.m
+++ GroupTrials.m
@@ -1,4 +1,5 @@
 
+% last update: 21.11.2017 by Bahareh: new condition added for uncertainty task 'CuePos & TargetPos & CurrEV & CurrVar (uncertainty 6 conditions)' 
 % last update: 20.11.2017 by Bahareh: new conditions for uncertainty task is added 'CuePos & TargetPos & PreGood & CurrVar (uncertainty)' 
 % last update: 18.11.2017 by Saeed: new conditions for uncertainty task is added 'CuePos & TargetPos & PreGood & CurrEV (uncertainty)' 
 % last update: 17.09.2017 by Bahareh: new condition added for 'CuePos & TargetPos & CurrEV & CurrVar' 
@@ -722,6 +723,23 @@ switch grType
         for gr = 1:size(allConditions,1)
             trlIdx = find((eventTable.cue_pos==allConditions(gr,1)) .* (eventTable.target_pos==allConditions(gr,2)) .* ...
                           (eventTable.pre_good==allConditions(gr,3)) .* (rewardVariance==allConditions(gr,4)));
+            output = [output; struct('TrialIdx', trlIdx, ...
+                                     'Value', allConditions(gr,:), ...
+                                     'GroupingType', grType)];
+            clear trlIdx                                 
+        end 
+        clear gr
+        
+
+    case 'CuePos & TargetPos & CurrEV & CurrVar (uncertainty 6 conditions)' 
+        % make a matrix with all possible combinations of these variables
+        allConditions = combvec([-1 1],[-1 1], [0.25 0.5 0.75] ,[0 1])'; % we assign variance 0 to R* and variance 1 to C* trial types 
+        output = [];
+        eventTable = struct2table(event);
+        rewardVariance = cellfun(@(x) (strcmp(x(1),'R')*0)+(strcmp(x(1),'C')*1), eventTable.type);
+        for gr = 1:size(allConditions,1)
+            trlIdx = find((eventTable.cue_pos==allConditions(gr,1)) .* (eventTable.target_pos==allConditions(gr,2)) .* ...
+                          (eventTable.expected_reward==allConditions(gr,3)) .* (rewardVariance==allConditions(gr,4)));
             output = [output; struct('TrialIdx', trlIdx, ...
                                      'Value', allConditions(gr,:), ...
                                      'GroupingType', grType)];
